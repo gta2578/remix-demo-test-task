90f8cc9208eff580c8871b740b23fdc0c3a2a98b
gta2578 <gta2578@gmail.com>
2024-11-08 19:11:29 +0200

    add React import in AppInputSwitch component, fix types in appLink component

--- app/global/components/app-link.tsx
+++ app/global/components/app-link.tsx
@@ -1,22 +1,26 @@
-import {LinkProps} from '@remix-run/react';
+import { LinkProps } from '@remix-run/react';
+import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material';
+import { I18nLink } from './i18n-link';
+import React from 'react';
 
-import {Link as MuiLink, LinkProps as MuiLinkProps} from '@mui/material';
-
-import {I18nLink} from './i18n-link';
-
-//
-//
-
-type MuiAppI18nLinkProps = LinkProps & Omit<MuiLinkProps, 'href'>;
+type MuiAppI18nLinkProps = Omit<LinkProps, 'to'> &
+  Omit<MuiLinkProps, 'href'> & {
+  to: string;
+  viewTransition?: boolean;
+};
 
 export const AppLink: React.FC<MuiAppI18nLinkProps> = ({
-  viewTransition = true,
-  children,
-  ...props
+ to,
+ viewTransition = true,
+ children,
+ ...props
 }: MuiAppI18nLinkProps) => {
   return (
-    // @ts-expect-error - `to` is not allowed in MuiLinkProps
-    <MuiLink viewTransition={viewTransition} LinkComponent={I18nLink} href={props.to} {...props}>
+    <MuiLink
+      component={I18nLink}
+      to={to}
+      {...props}
+    >
       {children}
     </MuiLink>
   );
